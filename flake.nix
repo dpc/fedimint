249eb776b677f86ef64b75fbec524c55247298d0
Dawid Ciężarkiewicz <dpc@dpc.pw>
2022-09-26 13:59:26 -0700

    Fix it

--- flake.nix
+++ flake.nix
@@ -37,18 +37,10 @@
 
         # Env vars we need for wasm32 cross compilation
         wasm32CrossEnvVars = ''
-          # Fix wasm32 compilation
-          # export LD_wasm32_unknown_unknown="${pkgs.llvmPackages_14.clang}/bin/clang"
-          # export LD_wasm32_unknown_unknown="${pkgs.gcc}/bin/cc"
-          # export LD_wasm32_unknown_unknown="${pkgs.llvmPackages_14.clang}/bin/ld"
-          # export LD_wasm32_unknown_unknown="${pkgs.llvmPackages_14.clang-unwrapped}/bin/clang-14"
-          # export LDFLAGS_wasm32_unknown_unknown="-dead_strip -L ${pkgs.glibc_multi}/lib"
-          #
         '' + (if isArch64Darwin then
           ''
-            export CC_wasm32_unknown_unknown="${pkgs.llvmPackages_14.clang-unwrapped}/bin/clang-14"
-            export CFLAGS_wasm32_unknown_unknown="-I ${pkgs.llvmPackages_14.libclang.lib}/lib/clang/14.0.1/include/"
-            export LD_wasm32_unknown_unknown="${pkgs.llvmPackages_13.bintools-unwrapped}/bin/wasm-ld"
+            export CC_wasm32_unknown_unknown=/opt/homebrew/opt/llvm/bin/clang
+            export AR_wasm32_unknown_unknown=/opt/homebrew/opt/llvm/bin/llvm-ar
           '' else
           ''
             export CC_wasm32_unknown_unknown="${pkgs.llvmPackages_14.clang-unwrapped}/bin/clang-14"
