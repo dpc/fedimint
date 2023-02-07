89badb5861f6b081b1b56e021aba7911358d83d0
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-02-06 16:06:20 -0800

    feat: This does not work.

--- fedimint-server/src/lib.rs
+++ fedimint-server/src/lib.rs
@@ -102,6 +102,18 @@ pub struct FedimintServer {
     pub decoders: ModuleDecoderRegistry,
 }
 
+// TODO: delete this after we convert the codebase to actually
+// use (and thus enforce) `Send` and `Sync` on `FedimintServer`
+const _: () = {
+    fn assert_send<T: Send>() {}
+
+    #[allow(path_statements)]
+    #[allow(clippy::no_effect)]
+    fn assert_all() {
+        assert_send::<FedimintServer>;
+    }
+};
+
 impl FedimintServer {
     /// Start all the components of the mint and plug them together
     pub async fn run(
