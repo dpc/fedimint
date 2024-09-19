79f3eca743c7a3ae80e30d737aaced4c8039af32
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-09-19 14:59:35 -0700

    chore: write a test

--- fedimint-server/src/consensus/aleph_bft/backup.rs
+++ fedimint-server/src/consensus/aleph_bft/backup.rs
@@ -1,7 +1,9 @@
 use async_trait::async_trait;
-use fedimint_core::db::{Database, IDatabaseTransactionOpsCoreTyped};
+use fedimint_core::db::{Database, IDatabaseTransactionOpsCoreTyped, IRawDatabaseExt};
+use fedimint_core::timing::{self, TimeReporter};
+use fedimint_logging::TracingSetup;
 use futures::StreamExt;
-use tracing::info;
+use tracing::{info, Level};
 
 use crate::consensus::db::{AlephUnitsKey, AlephUnitsPrefix};
 use crate::LOG_CONSENSUS;
@@ -67,4 +69,31 @@ impl aleph_bft::BackupWriter for BackupWriter {
 
         Ok(())
     }
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn backup_writer_perf() -> anyhow::Result<()> {
+    // Ensure tracing has been set once
+    let _ = TracingSetup::default().init();
+
+    let db = fedimint_rocksdb::RocksDb::open("../fedimint-dbg-copy-by-dpc/database")?;
+
+    let db: Database = db.into_database();
+
+    let _timing: TimeReporter /* logs on drop */ = timing::TimeReporter::new("alephbft-backup-writer-append").level(
+        Level::DEBUG);
+
+    let mut dbtx = db.begin_transaction().await;
+
+    let index = dbtx
+        .find_by_prefix_sorted_descending(&AlephUnitsPrefix)
+        .await
+        .next()
+        .await
+        .map_or(0, |entry| dbg!(entry.0 .0) + 1);
+
+    drop(_timing);
+
+    assert_eq!(index, 2484);
+    Ok(())
 }
